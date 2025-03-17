d68224f6806de598d00428926bcfd7ee6a45d4ef
harrisonr98 <harrisonr98@yahoo.com>
2025-03-17 14:24:43 -0400

    added responsive styles, removed unused components, added global text styles, added global button styles, built accordion component, added team section & tiles, added CTA section.

--- src/lib/Components/NavButton.svelte
+++ src/lib/Components/NavButton.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <button onclick={toggleMenu} class="text-gray-700 focus:outline-none" aria-label="Toggle menu">
-	<svg class="pointer-events-none h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+	<svg class="pointer-events-none h-[40px] w-[40px]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 		{#if isMenuOpen}
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"
 			></path>
