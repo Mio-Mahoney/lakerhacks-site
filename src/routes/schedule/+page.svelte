fe4af36b8293949c72f163773c7c6a29ce13ef02
SamanthaJeanneb <123563481+SamanthaJeanneb@users.noreply.github.com>
2025-03-31 02:55:02 -0400

    Update +page.svelte

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -22,20 +22,20 @@
 	<div class="relative w-full aspect-w-16 aspect-h-9">
 		<iframe 
 			loading="lazy" 
-			class="absolute w-full h-full top-0 left-0 " 
+			class="absolute w-full h-full top-0 left-0" 
 			src="https://www.canva.com/design/DAGjRb98rrU/yjs0HQPsoI5kxb1Q0_r5wQ/view?embed" 
 			allowfullscreen="allowfullscreen" 
 			allow="fullscreen">
 		</iframe>
 	</div>
-    <a 
-    href="https://www.canva.com/design/DAGjRb98rrU/yjs0HQPsoI5kxb1Q0_r5wQ/view?utm_content=DAGjRb98rrU&utm_campaign=designshare&utm_medium=embeds&utm_source=link" 
-    target="_blank" 
-    rel="noopener" 
-    class="rounded-md bg-[#0B111F] border border-[#D4563F] px-6 py-2 text-sm font-medium text-[#D4563F] transition-all hover:bg-[#050a14]"
-    >
-    View Full Schedule
-</a>
+
+	<a 
+		href="https://www.canva.com/design/DAGjRb98rrU/yjs0HQPsoI5kxb1Q0_r5wQ/view?utm_content=DAGjRb98rrU&utm_campaign=designshare&utm_medium=embeds&utm_source=link" 
+		target="_blank" 
+		rel="noopener" 
+		class="rounded-md bg-[#0B111F] border border-[#D4563F] px-6 py-2 text-sm font-medium text-[#D4563F] transition-all hover:bg-[#050a14]">
+		View Full Schedule
+	</a>
 </main>
 
 <style>
@@ -50,8 +50,8 @@
 	@media (max-width: 640px) {
 		.aspect-w-16.aspect-h-9 {
 			padding-top: 130%; 
-            width: 100%;
-            max-width: 100%;
+			width: 100%;
+			max-width: 100%;
 		}
 	}
 </style>
