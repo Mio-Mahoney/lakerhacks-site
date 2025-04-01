9ea0e6851454c9f5330e01c7e41d5c16e972009e
harrisonr98 <harrisonr98@yahoo.com>
2025-04-01 13:23:02 -0400

    fixed schedule page

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -8,29 +8,9 @@
 </svelte:head>
 
 <main class="flex min-h-screen flex-col items-center bg-[#0B111F]">
-	<div class="relative hidden w-full md:block">
-		<iframe
-			loading="lazy"
-			class="absolute top-0 left-0 h-full w-full"
-			src="https://www.canva.com/design/DAGjR2YZHyo/_BcygR-hW3YWwrW-_mctQA/view?embed"
-			allowfullscreen="allowfullscreen"
-			allow="fullscreen"
-		>
-		</iframe>
+	<div class="w-full max-w-[600px]">
+		<img src={scheduleImg} alt="schedule" class="w-full" />
 	</div>
-
-	<div class="relative block w-full md:hidden">
-		<img src={scheduleImg} alt="schedule" />
-	</div>
-
-	<a
-		href="https://www.canva.com/design/DAGjRb98rrU/yjs0HQPsoI5kxb1Q0_r5wQ/view?utm_content=DAGjRb98rrU&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
-		target="_blank"
-		rel="noopener"
-		class="rounded-md border border-[#D4563F] bg-[#0B111F] px-6 py-2 text-sm font-medium text-[#D4563F] transition-all hover:bg-[#050a14]"
-	>
-		View Full Schedule
-	</a>
 </main>
 
 <style>
